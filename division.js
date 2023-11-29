3aeed616b1dae3ed04814a303313dd80e31a7cba
iamvik7 <151925399+iamvik7@users.noreply.github.com>
2023-11-29 12:06:50 +0530

    Create division.js
    ## Description 
    
    Added a new feature to divide two numbers.
    
    Added a new file `divison.js` this as like previous file take two numbers and a function findDivison(a, b) that checks the condition that number should not be zero or negative.

---
+++ division.js
@@ -0,0 +1,18 @@
+//variables
+let a = 102;
+let b = 20;
+
+//function dividing two numbers
+function findDivison(a, b) {
+  if(a > b && a > 0 && b > 0) {
+    return a/b;
+  } else if(b > a && b > 0 && a > 0) {
+    return b/a;
+  }
+}
+
+//Answer
+let ans = findDivison(a, b);
+
+//Print answer to user
+console.log(and);
